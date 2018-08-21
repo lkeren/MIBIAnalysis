ff614883b2bf6299ae816174d239be968d917a4c
Hazmatius <alexbaranski@gmail.com>
2018-08-21 13:56:43 -0700

    Fixes cap bug and adds log

--- GUI_util/MIBI_remove_background.m
+++ GUI_util/MIBI_remove_background.m
@@ -6,18 +6,22 @@ function [] = MIBI_remove_background()
     gausRad = pipeline_data.gausRad;
     t = pipeline_data.t;
     removeVal = pipeline_data.removeVal;
-    cap = pipeline_data.cap;
+    capBgChannel = pipeline_data.capBgChannel;
+    capEvalChannel = pipeline_data.capEvalChannel;
 
     for i=1:length(corePath)
         disp(['Working on ' num2str(i), '...']);
         [countsAllSFiltCRSum, labels] = load_tiff_data(corePath{i});
         [~,bgChannelInd] = ismember(bgChannel,labels);
-        mask = MIBI_get_mask(countsAllSFiltCRSum(:,:,bgChannelInd),cap,t,gausRad,0,'');
+        mask = MIBI_get_mask(countsAllSFiltCRSum(:,:,bgChannelInd),capBgChannel,t,gausRad,0,'');
         countsNoBg = MibiRemoveBackgroundByMaskAllChannels(countsAllSFiltCRSum,mask,removeVal);
         save ([corePath{i},'dataNoBg.mat'],'countsNoBg');
         [savePath, ~, ~] = fileparts(corePath{i});
         MibiSaveTifs ([savePath,'/TIFsNoBg/'], countsNoBg, labels)
     end
+    fid = fopen([savePath, '/[', datestr(datetime('now')), ']_background_removal.log'], 'wt');
+    fprintf(fid, 'background channel: %s\nbackground cap: %f\nevaluation cap: %f\ngaussian radius: %f\nthreshold: %f\nremove value: %f', bgChannel, capBgChannel, capEvalChannel, gausRad, t, removeVal);
+    fclose(fid);
     disp("Finished removing background.");
 end
 
