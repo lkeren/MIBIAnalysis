e853a1f10cea8514da760b5388868de7d5b0eb3e
Hazmatius <alexbaranski@gmail.com>
2018-10-10 14:18:59 -0700

    Ignores hidden “TIF” files

--- GUI/gui_lib/tiff_lib/loadTIFF_folder.m
+++ GUI/gui_lib/tiff_lib/loadTIFF_folder.m
@@ -1,26 +1,28 @@
 function [counts, labels, tags] = loadTIFF_folder(path)
-    % First we look for all TIFF files in the given path
-    fileList = [dir(fullfile(path, '*.tiff'));...
-                dir(fullfile(path, '*.tif'))];
-    if isempty(fileList)
-        error(['No TIFF files found in ', path]);
-    end
-    files = {fileList.name}';
-    rmIdx = [find(strcmp(files, 'totalIon.tif')), find(strcmp(files, 'totalIon.tiff'))];
-    files(rmIdx) = []; % removes totalIon.tif and/or totalIon.tiff
-    num_pages = numel(files);
-    counts = [];
-    labels = {};
-    tags = {};
-    for index=1:num_pages
-        tiff = Tiff([path, filesep, files{index}]);
-        counts(:,:,index) = read(tiff);
-        tags{index} = getTagStruct(tiff);
-        try
-            desc = json.load(tags{index}.ImageDescription);
-            labels{index} = desc.channel0x2Etarget;
-        catch
-            [~, labels{index}, ~] = fileparts(files{index});
-        end
-    end
+   % First we look for all TIFF files in the given path
+   fileList = [dir(fullfile(path, '*.tiff'));...
+               dir(fullfile(path, '*.tif'))];
+   if isempty(fileList)
+       error(['No TIFF files found in ', path]);
+   end
+   files = {fileList.name}';
+   rmIdx = [find(strcmp(files, 'totalIon.tif')), find(strcmp(files, 'totalIon.tiff'))];
+   files(rmIdx) = []; % removes totalIon.tif and/or totalIon.tiff
+   rmIdx = find(cellfun(@isHiddenName, {fileList.name}));
+   files(rmIdx) = [];
+   num_pages = numel(files);
+   counts = [];
+   labels = {};
+   tags = {};
+   for index=1:num_pages
+       tiff = Tiff([path, filesep, files{index}]);
+       counts(:,:,index) = read(tiff);
+       tags{index} = getTagStruct(tiff);
+       try
+           desc = json.load(tags{index}.ImageDescription);
+           labels{index} = desc.channel0x2Etarget;
+       catch
+           [~, labels{index}, ~] = fileparts(files{index});
+       end
+   end
 end
